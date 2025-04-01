d4992bf7f1d0c8742036ebe1c9377d9982fa60af
Jonathan Hutchison <jbhutch01@gmail.com>
2025-04-01 11:27:39 -0400

    Update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,9 @@
 import antfu from '@antfu/eslint-config'
+// import tailwind from 'eslint-plugin-tailwindcss'
 
 export default antfu({
   react: true,
   ignores: ['**/**.gen.*'],
-})
+},
+  // ...tailwind.configs['flat/recommended'], // Not compatible with Tailwind v4
+)
