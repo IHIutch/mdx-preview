e3323ef1b3ea4b771ebacb07b51a1ed9b2e198a8
Jonathan Hutchison <jbhutch01@gmail.com>
2025-04-17 17:13:10 -0400

    Update uswds

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,12 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu({
   react: true,
-  ignores: ['**/**.gen.*'],
+  typescript: true,
+  stylistic: {
+    indent: 2,
+    quotes: 'single',
+  },
+  ignores: ['**/**.gen.*', '**/uswds/**'],
 },
   // ...tailwind.configs['flat/recommended'], // Not compatible with Tailwind v4
 )
